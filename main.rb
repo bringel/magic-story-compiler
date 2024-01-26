00bbc1e7b2e9b6e782e699892982b608cbf75c9e
Brad Ringel <brad.ringel@gmail.com>
2024-01-26 15:08:28 -0500

    Check other years using the year slider and click on main and side stories

--- main.rb
+++ main.rb
@@ -12,15 +12,26 @@ class MagicStoryCompiler
 
   def get_article_links(set_name:)
     find_set_element(set_name: set_name).click
-    find_article_elements.map do |el|
-      title = el.find('h3').text(:all)
-      link = el.find('a[aria-label="Read More"]')
 
-      {
-        title: title,
-        url: link['href']
-      }
+    find_story_switch_buttons.flat_map do |b|
+      b.click
+      find_article_elements.map do |el|
+        title = el.find('h3').text(:all)
+        link = el.find('a[aria-label="Read More"]')
+
+        {
+          title: title,
+          url: link['href']
+        }
+      end
     end
+  end
+
+  def find_story_switch_buttons()
+    [
+      find_story_archive.find_button("Magic Story"),
+      find_story_archive.find_button("Side Stories")
+    ]
   end
   
   def find_article_elements
@@ -28,9 +39,26 @@ class MagicStoryCompiler
   end
   
   def find_set_element(set_name:)
-    s = find_set_slider
-    # TODO: go back to previous years
-    s.find("div.swiper-slide", text: set_name) 
+    sets = find_set_slider
+    years = find_year_slider
+
+    years.all("span.swiper-slide").each do |y|
+      y.click
+      begin
+        set = sets.find("div.swiper-slide", text: set_name) 
+      rescue Capybara::ElementNotFound
+        next
+      end
+      return set if set
+    end
+  end
+
+  def find_year_slider()
+    scroll_to(find_story_archive)
+    has_selector?("section#story-arcive div.swiper-wrapper")     
+    sliders = all("section#story-archive div.swiper-wrapper")
+  
+    sliders.find { |s| s.all("span", text: 2023).length > 0 }
   end
 
   def find_set_slider()
