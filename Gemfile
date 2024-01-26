7ac9e4b2e5532d1f21fff5d454defdf0b4238982
Brad Ringel <brad.ringel@gmail.com>
2024-01-26 17:15:03 -0500

    go to the artile pages and get the text and metadata

--- Gemfile
+++ Gemfile
@@ -8,3 +8,6 @@ gem "nokogiri", "~> 1.16"
 gem "capybara", "~> 3.39"
 
 gem "selenium-webdriver", "~> 4.17"
+
+gem "loofah", "~> 2.22"
+
--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'capybara/dsl'
+require 'loofah';
 
 class MagicStoryCompiler
   include Capybara::DSL
@@ -8,6 +9,44 @@ class MagicStoryCompiler
 
   def initialize()
     visit("https://magic.wizards.com/en/story")
+  end
+
+  def get_all_set_story_articles(set_name:)
+    links = get_article_links(set_name: set_name)
+    links.map do |link|
+      get_story_article(story_hash: link)
+    end
+  end
+
+  def get_story_article(story_hash:)
+    visit(story_hash[:url])
+
+    main_article = find("article")
+    header = main_article.find("header")
+    body = main_article.find("div.article-body")
+
+    publish_date = header.find("time").text
+
+    author = header.all("a").find { |e| e.text != 'Magic Story' }.text
+
+    body_text = body['innerHTML']
+
+    # after we scrub out any unsafe tags, there can be empty paragraphs or horizontal rules with nothing above them
+    # and this scrubber will remove them
+    empty_scrubber = Loofah::Scrubber.new do |node|
+      if node.name == 'p'  && node.text.strip == ""
+        node.remove
+      elsif node.name == 'text' && node.text.strip == "" && node.previous.nil?
+        node.remove
+      elsif node.name == 'hr' && node.previous.nil?
+        node.remove
+      end
+      Loofah::Scrubber::STOP
+    end
+
+    clean_body_text = Loofah.html5_fragment(body_text).scrub!(:prune).scrub!(empty_scrubber).to_s
+
+    story_hash.merge(author: author, publish_date: publish_date, text: clean_body_text)
   end
 
   def get_article_links(set_name:)
@@ -28,10 +67,15 @@ class MagicStoryCompiler
   end
 
   def find_story_switch_buttons()
+    main_story_button = find_story_archive.find_button("Magic Story")
+    begin
+      side_story_button = find_story_archive.find_button("Side Stories")
+    rescue Capybara::ElementNotFound
+    end
     [
-      find_story_archive.find_button("Magic Story"),
-      find_story_archive.find_button("Side Stories")
-    ]
+      main_story_button,
+      side_story_button
+    ].compact
   end
   
   def find_article_elements
